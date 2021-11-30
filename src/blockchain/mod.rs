5608e56f7934a6683ffb39a0bee58408b776f028
John Cantrell <johncantrell97@gmail.com>
2021-11-30 17:56:47 -0500

    [blockchain] Add IndexedChain trait for accessing chain data

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -21,7 +21,7 @@ use std::ops::Deref;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::Arc;
 
-use bitcoin::{BlockHash, Transaction, Txid};
+use bitcoin::{BlockHeader, Script, Transaction, Txid};
 
 use crate::database::BatchDatabase;
 use crate::error::Error;
@@ -83,6 +83,32 @@ pub enum Capability {
     GetAnyTx,
     /// Can compute accurate fees for the transactions found during sync
     AccurateFees,
+}
+
+/// The height and confirmation status of a transaction
+pub struct TxStatus {
+    /// Whether the transaction has at least one confirmation
+    pub confirmed: bool,
+    /// The height of the block the transaction was confirmed in
+    pub block_height: Option<u32>,
+}
+
+/// A trait Blockchains can implement if they support querying chain data
+#[maybe_async]
+pub trait IndexedChain {
+    /// Get the block header for a given block height
+    fn get_header(&self, height: u32) -> Result<BlockHeader, Error>;
+
+    /// Get the position of a specific transaction in a block
+    fn get_position_in_block(&self, txid: &Txid, height: usize) -> Result<Option<usize>, Error>;
+
+    /// Get the confirmation status and height of a transaction by Txid
+    fn get_tx_status(&self, txid: &Txid) -> Result<Option<TxStatus>, Error>;
+
+    /// Get all transactions that spend or fund a certain Script
+    /// Includes the confirmation status and height for each transaction
+    fn get_script_tx_history(&self, script: &Script)
+        -> Result<Vec<(TxStatus, Transaction)>, Error>;
 }
 
 /// Trait that defines the actions that must be supported by a blockchain backend
